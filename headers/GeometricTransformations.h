10c712af041a0ffd2b6ccc8b25f6639adba191f5
Michal Dubovsky <michal.dubovsky@cern.ch>
2023-10-02 11:16:18 +0200

    Optimizing geometric transformation class

--- headers/GeometricTransformations.h
+++ headers/GeometricTransformations.h
@@ -10,13 +10,16 @@ namespace AstroPhotoStacker {
                 m_shift_y(shift_y),
                 m_rotation(rotation),
                 m_rotation_center_x(rotation_center_x),
-                m_rotation_center_y(rotation_center_y) {};
+                m_rotation_center_y(rotation_center_y) {
+                    cosx = cos(m_rotation);
+                    sinx = sin(m_rotation);
+                };
 
             void transform_to_reference_frame(float *x, float *y)   const   {
                 float x_new = *x - m_rotation_center_x;
                 float y_new = *y - m_rotation_center_y;
-                *x = x_new*cos(m_rotation) - y_new*sin(m_rotation) + m_rotation_center_x + m_shift_x;
-                *y = x_new*sin(m_rotation) + y_new*cos(m_rotation) + m_rotation_center_y + m_shift_y;
+                *x = x_new*cosx - y_new*sinx + m_rotation_center_x + m_shift_x;
+                *y = x_new*sinx + y_new*cosx + m_rotation_center_y + m_shift_y;
             };
 
         private:
@@ -25,5 +28,7 @@ namespace AstroPhotoStacker {
             float m_rotation;
             float m_rotation_center_x;
             float m_rotation_center_y;
+
+            float cosx, sinx;
     };
 }
