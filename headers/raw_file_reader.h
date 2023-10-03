35db3451de48a9336ba9f8fa7df974620ed5886a
Michal Dubovsky <michal.dubovsky@cern.ch>
2023-10-03 21:24:57 +0200

    Namespace

--- headers/raw_file_reader.h
+++ headers/raw_file_reader.h
@@ -6,52 +6,55 @@
 #include <iostream>
 #include <vector>
 
-template<typename output_type = unsigned short>
-std::unique_ptr<output_type[]> read_raw_file(const std::string &raw_file_address, int *width, int *height, std::vector<char> *colors = nullptr)   {
-    // create a LibRaw object
-    LibRaw rawProcessor;
+namespace AstroPhotoStacker   {
 
-    // open the file
-    if (rawProcessor.open_file(raw_file_address.c_str()) != LIBRAW_SUCCESS) {
-        throw std::runtime_error("Cannot open raw file " + raw_file_address);
-    }
+    template<typename output_type = unsigned short>
+    std::unique_ptr<output_type[]> read_raw_file(const std::string &raw_file_address, int *width, int *height, std::vector<char> *colors = nullptr)   {
+        // create a LibRaw object
+        LibRaw rawProcessor;
 
-    // unpack the raw data
-    if (rawProcessor.unpack() != LIBRAW_SUCCESS) {
-        throw std::runtime_error("Cannot unpack raw data from file " + raw_file_address);
-    }
+        // open the file
+        if (rawProcessor.open_file(raw_file_address.c_str()) != LIBRAW_SUCCESS) {
+            throw std::runtime_error("Cannot open raw file " + raw_file_address);
+        }
 
-    rawProcessor.raw2image();
+        // unpack the raw data
+        if (rawProcessor.unpack() != LIBRAW_SUCCESS) {
+            throw std::runtime_error("Cannot unpack raw data from file " + raw_file_address);
+        }
 
-    int col, bps;
-    rawProcessor.get_mem_image_format(width, height, &col, &bps);
+        rawProcessor.raw2image();
 
-    std::unique_ptr<output_type[]> brightness = std::unique_ptr<output_type[]>(new output_type[(*width)*(*height)]);
-    if (colors != nullptr)   {
-        colors->resize((*width)*(*height));
-    }
-    const unsigned short int (*image_data)[4] = rawProcessor.imgdata.image;
+        int col, bps;
+        rawProcessor.get_mem_image_format(width, height, &col, &bps);
 
-    if (colors != nullptr)   {
-        for (int row = 0; row < *height; ++row) {
-            for (int col = 0; col < *width; ++col) {
-                const unsigned int index = row * (*width) + col;
-                brightness[index] = image_data[index][rawProcessor.COLOR(row,col)];
-                (*colors)[index] = rawProcessor.COLOR(row,col);
+        std::unique_ptr<output_type[]> brightness = std::unique_ptr<output_type[]>(new output_type[(*width)*(*height)]);
+        if (colors != nullptr)   {
+            colors->resize((*width)*(*height));
+        }
+        const unsigned short int (*image_data)[4] = rawProcessor.imgdata.image;
+
+        if (colors != nullptr)   {
+            for (int row = 0; row < *height; ++row) {
+                for (int col = 0; col < *width; ++col) {
+                    const unsigned int index = row * (*width) + col;
+                    brightness[index] = image_data[index][rawProcessor.COLOR(row,col)];
+                    (*colors)[index] = rawProcessor.COLOR(row,col);
+                }
             }
         }
-    }
-    else    {
-        for (int row = 0; row < *height; ++row) {
-            for (int col = 0; col < *width; ++col) {
-                const unsigned int index = row * (*width) + col;
-                brightness[index] = image_data[index][rawProcessor.COLOR(row,col)];
+        else    {
+            for (int row = 0; row < *height; ++row) {
+                for (int col = 0; col < *width; ++col) {
+                    const unsigned int index = row * (*width) + col;
+                    brightness[index] = image_data[index][rawProcessor.COLOR(row,col)];
+                }
             }
         }
-    }
 
-    // close the file
-    rawProcessor.recycle();
+        // close the file
+        rawProcessor.recycle();
 
-    return brightness;
-};
+        return brightness;
+    };
+}
--- src/StarFinder.cxx
+++ src/StarFinder.cxx
@@ -1,14 +1,15 @@
 #include "../headers/StarFinder.h"
 
 using namespace std;
+using namespace AstroPhotoStacker;
 
-void sort_stars_by_size(std::vector<std::tuple<float, float,int> > *stars) {
+void AstroPhotoStacker::sort_stars_by_size(std::vector<std::tuple<float, float,int> > *stars) {
     std::sort(stars->begin(), stars->end(), [](const std::tuple<float, float,int> &a, const std::tuple<float, float,int> &b) {
         return std::get<2>(a) > std::get<2>(b);
     });
 };
 
-void keep_only_stars_above_size(std::vector<std::tuple<float, float,int> > *stars, int min_size) {
+void AstroPhotoStacker::keep_only_stars_above_size(std::vector<std::tuple<float, float,int> > *stars, int min_size) {
     stars->erase(std::remove_if(stars->begin(), stars->end(), [min_size](const std::tuple<float, float,int> &star) {
         return std::get<2>(star) < min_size;
     }), stars->end());
