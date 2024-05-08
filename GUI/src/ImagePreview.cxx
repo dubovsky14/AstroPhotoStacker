a98285289b720d4ede3e3b7b33a2cd18c591c24f
dubovsky14 <dubovsky14@gmail.com>
2024-05-08 23:45:40 +0200

    Removing duplicit code

--- GUI/src/ImagePreview.cxx
+++ GUI/src/ImagePreview.cxx
@@ -26,14 +26,7 @@ void ImagePreview::get_preview_from_stacked_picture(const std::vector<std::vecto
                                                     int width_resized, int height_resized,
                                                     int *max_value) {
 
-    m_preview_data = std::vector<std::vector<int>>(3, std::vector<int>(width_resized*height_resized,0)); // 2D vector of brightness values - first index = color, second index = pixel
-    std::vector<int>                count(width_resized*height_resized,0);
-
-    const float step_x = width_original / float(width_resized);
-    const float step_y =  height_original / float(height_resized);
-
-    m_width_original = width_original;
-    m_height_original = height_original;
+    m_current_preview_is_raw_file = false;
     m_original_image = std::vector<std::vector<int>>(3, std::vector<int>(width_original*height_original,0));
     for (int i_color = 0; i_color < 3; i_color++)   {
         for (int i_pixel = 0; i_pixel < width_original*height_original; i_pixel++)   {
@@ -41,29 +34,7 @@ void ImagePreview::get_preview_from_stacked_picture(const std::vector<std::vecto
         }
     }
 
-
-    for (int i_color = 0; i_color < 3; i_color++)   {
-        for (int i_original_y = 0; i_original_y < height_original; i_original_y++)  {
-            for (int i_original_x = 0; i_original_x < width_original ; i_original_x++)  {
-                const int index = i_original_y * width_original  + i_original_x;
-                const int i_pixel_new_y = i_original_y / step_y;
-                const int i_pixel_new_x = i_original_x / step_x;
-                const int index_new = i_pixel_new_y * width_resized + i_pixel_new_x;
-                m_preview_data[i_color][index_new] += m_original_image[i_color][index];
-                count[index_new]++;
-            }
-        }
-    }
-
-    *max_value = 0;
-    for (int i_color = 0; i_color < 3; i_color++)   {
-        for (int i_pixel = 0; i_pixel < width_resized*height_resized; i_pixel++)   {
-            if (count[i_pixel] > 0) {
-                m_preview_data[i_color][i_pixel] /= count[i_pixel];
-            }
-            *max_value = max<int>(*max_value, m_preview_data[i_color][i_pixel]);
-        }
-    }
+    update_preview_data();
 };
 
 void ImagePreview::read_preview_from_raw_file(const std::string &path)  {
