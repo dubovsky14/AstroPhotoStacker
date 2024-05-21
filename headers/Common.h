39a2a6e5edb98e13ea3865b89846d2a8b26ec359
dubovsky14 <dubovsky14@gmail.com>
2024-05-21 21:03:26 +0200

    Doxygen for Common.h

--- headers/Common.h
+++ headers/Common.h
@@ -6,71 +6,208 @@
 
 namespace AstroPhotoStacker {
 
+    /**
+     * @brief Generate a random number between 0 and 1
+     *
+     * @return float The generated random number
+    */
     float random_uniform();
 
+    /**
+     * @brief Calculate 2nd root of a number
+     *
+     * @tparam X The type of the number
+     * @param x The number to calculate the 2nd root of
+     * @return X The 2nd root of the number
+    */
     template <class X>
     inline X pow2(X x) {return x*x;};
 
+    /**
+     * @brief Check if the main_string contains the substring
+     *
+     * @param main_string The main string to search in
+     * @param substring The substring to search for
+     * @return bool True if the main_string contains the substring, false otherwise
+    */
     bool contains(const std::string &main_string, const std::string &substring);
 
+    /**
+     * @brief Check if the main_string starts with the prefix
+     *
+     * @param main_string The main string to check
+     * @param prefix The prefix to check for
+     * @return bool True if the main_string starts with the prefix, false otherwise
+    */
     bool starts_with(const std::string &main_string, const std::string &prefix);
 
+    /**
+     * @brief Check if the main_string ends with the suffix
+     *
+     * @param main_string The main string to check
+     * @param suffix The suffix to check for
+     * @return bool True if the main_string ends with the suffix, false otherwise
+    */
     bool ends_with(const std::string &main_string, const std::string &suffix);
 
+    /**
+     * @brief Check if the string represents an integer
+     *
+     * @param x The string to check
+     * @return bool True if the string represents an integer, false otherwise
+    */
     bool string_is_int(const std::string &x);
 
+    /**
+     * @brief Check if the string represents a floating-point number
+     *
+     * @param x The string to check
+     * @return bool True if the string represents a floating-point number, false otherwise
+    */
     bool string_is_float(const std::string &x);
 
+    /**
+     * @brief Split and strip a string based on a separator
+     *
+     * @param input_string The input string to split and strip
+     * @param separator The separator to split the string by
+     * @return std::vector<std::string> The vector of split and stripped strings
+    */
     std::vector<std::string> split_and_strip_string(std::string input_string, const std::string &separator);
 
+    /**
+     * @brief Split a string based on a separator
+     *
+     * @param input_string The input string to split
+     * @param separator The separator to split the string by
+     * @return std::vector<std::string> The vector of split strings
+    */
     std::vector<std::string> split_string(std::string input_string, const std::string &separator);
 
+    /**
+     * @brief Split a string by whitespaces
+     *
+     * @param line The input string to split
+     * @return std::vector<std::string> The vector of split strings
+    */
     std::vector<std::string> split_by_whitespaces(std::string line);
 
+    /**
+     * @brief Strip characters from a string
+     *
+     * @param input_string The input string to strip
+     * @param chars_to_remove The characters to remove from the string - default are whitespaces
+    */
     void strip_string(std::string *input_string, const std::string &chars_to_remove = " \n\t\r");
 
-    void         to_upper(std::string *input_string);
+    /**
+     * @brief Convert a string to uppercase in-place
+     *
+     * @param input_string The input string to convert to uppercase
+    */
+    void to_upper(std::string *input_string);
 
-    std::string  to_upper_copy(const std::string &input_string);
+    /**
+     * @brief Convert a string to uppercase and return a copy
+     *
+     * @param input_string The input string to convert to uppercase
+     * @return std::string The uppercase string
+    */
+    std::string to_upper_copy(const std::string &input_string);
 
-    // convert both strings to uppercase and compare
+    /**
+     * @brief Compare two strings in a case-insensitive manner
+     *
+     * @param x The first string to compare
+     * @param y The second string to compare
+     * @return bool True if the strings are equal (ignoring case), false otherwise
+    */
     bool compare_case_insensitive(const std::string &x, const std::string &y);
 
+    /**
+     * @brief Convert a string to the specified type
+     *
+     * @tparam ResultType The type to convert the string to
+     * @param input_string The input string to convert
+     * @return ResultType The converted value
+    */
     template <class ResultType>
     ResultType convert_string_to(const std::string &input_string) {
         throw std::runtime_error ("Requested type not implemented!");
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to an integer
+     *
+     * @param input_string The input string to convert
+     * @return int The converted integer value
+    */
     template <> inline
     int convert_string_to(const std::string &input_string)    {
         return std::stoi(input_string);
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to an unsigned integer
+     *
+     * @param input_string The input string to convert
+     * @return unsigned int The converted unsigned integer value
+    */
     template <> inline
     unsigned int convert_string_to(const std::string &input_string)    {
         return std::stoul(input_string);
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to an unsigned long long integer
+     *
+     * @param input_string The input string to convert
+     * @return unsigned long long int The converted unsigned long long integer value
+    */
     template <> inline
     unsigned long long int convert_string_to(const std::string &input_string)    {
         return std::stoull(input_string);
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to a float
+     *
+     * @param input_string The input string to convert
+     * @return float The converted float value
+    */
     template <> inline
     float convert_string_to(const std::string &input_string)    {
         return std::stod(input_string);
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to a double
+     *
+     * @param input_string The input string to convert
+     * @return double The converted double value
+    */
     template <> inline
     double convert_string_to(const std::string &input_string)    {
         return std::stod(input_string);
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to a string (no conversion needed)
+     *
+     * @param input_string The input string to convert
+     * @return std::string The input string itself
+    */
     template <> inline
     std::string convert_string_to(const std::string &input_string)    {
         return input_string;
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to a boolean
+     *
+     * @param input_string The input string to convert
+     * @return bool The converted boolean value
+    */
     template <> inline
     bool convert_string_to(const std::string &input_string)    {
         const std::string input_upper = AstroPhotoStacker::to_upper_copy(input_string);
@@ -81,6 +218,12 @@ namespace AstroPhotoStacker {
         }
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to a vector of integers
+     *
+     * @param input_string The input string to convert
+     * @return std::vector<int> The converted vector of integers
+    */
     template <> inline
     std::vector<int> convert_string_to(const std::string &input_string)    {
         std::string stripped_string = input_string;
@@ -94,6 +237,12 @@ namespace AstroPhotoStacker {
         return result;
     };
 
+    /**
+     * @brief Specialization of convert_string_to for converting a string to a vector of unsigned long long integers
+     *
+     * @param input_string The input string to convert
+     * @return std::vector<unsigned long long int> The converted vector of unsigned long long integers
+    */
     template <> inline
     std::vector<unsigned long long int> convert_string_to(const std::string &input_string)    {
         std::string stripped_string = input_string;
@@ -107,10 +256,32 @@ namespace AstroPhotoStacker {
         return result;
     };
 
+    /**
+     * @brief Get the list of raw files in a folder
+     *
+     * @param folder_address The address of the folder
+     * @return std::vector<std::string> The vector of raw file names in the folder
+    */
     std::vector<std::string> get_raw_files_in_folder(const std::string &folder_address);
 
+    /**
+     * @brief Round a number and convert it to a string
+     *
+     * @param x The number to round and convert
+     * @param digits_after_decimal_point The number of digits after the decimal point in the resulting string
+     * @return std::string - string representation of the rounded number
+    */
     std::string round_and_convert_to_string(double x, int digits_after_decimal_point = 1);
 
+    /**
+     * @brief Force a value to be within a specified range
+     *
+     * @tparam T The type of the value
+     * @param x The value to force within the range
+     * @param min_value The minimum value of the range
+     * @param max_value The maximum value of the range
+     * @return T The value forced within the range
+    */
     template <class T>
     T force_range(T x, T min_value, T max_value) {
         return std::max(min_value, std::min(max_value, x));
