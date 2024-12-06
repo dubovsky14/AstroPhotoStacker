f1c669a83ebb4116dcfe6d8af45cc8d6234c50af
dubovsky14 <dubovsky14@gmail.com>
2024-12-06 23:07:07 +0100

    Improving RGB alignment

--- headers/RGBAlignmentTool.h
+++ headers/RGBAlignmentTool.h
@@ -2,6 +2,8 @@
 
 #include "../headers/ImageFilesInputOutput.h"
 
+#include <opencv2/opencv.hpp>
+
 #include <vector>
 #include <string>
 
@@ -15,65 +17,41 @@ namespace AstroPhotoStacker {
         public:
             RGBAlignmentTool() = delete;
 
-            RGBAlignmentTool(const std::vector<std::vector<PixelType>> &original_image, unsigned int width, unsigned height)    {
-                m_data_original = original_image;
-                m_data_shifted = std::vector<std::vector<PixelType>>(3, std::vector<PixelType>(width*height, 0));
-                m_width = width;
-                m_height = height;
-            };
-
             RGBAlignmentTool(const std::string &file_address)   {
-                m_data_original = read_rgb_image<PixelType>(InputFrame(file_address), &m_width, &m_height);
-                m_data_shifted = std::vector<std::vector<PixelType>>(3, std::vector<PixelType>(m_width*m_height, 0));
+                m_data_original = cv::imread(file_address, cv::IMREAD_COLOR);
+                m_data_shifted = m_data_original.clone();
             };
 
             void calculate_shifted_image(const std::pair<int,int> &shift_red, const std::pair<int,int> &shift_blue) {
-                m_data_shifted = std::vector<std::vector<PixelType>>(3, std::vector<PixelType>(m_width*m_height, 0));
-                for (int x = 0; x < m_width; x++) {
-                    const int x_shifted_red = x + shift_red.first;
-                    const int x_shifted_blue = x + shift_blue.first;
-                    for (int y = 0; y < m_height; y++) {
-                        const int y_shifted_red = y + shift_red.second;
-                        const int y_shifted_blue = y + shift_blue.second;
+                cv::Mat trans_mat_red = (cv::Mat_<double>(2,3) << 1, 0, shift_red.first, 0, 1, shift_red.second);
+                cv::Mat trans_mat_blue = (cv::Mat_<double>(2,3) << 1, 0, shift_blue.first, 0, 1, shift_blue.second);
 
-                        if (x_shifted_red >= 0 && x_shifted_red < m_width && y_shifted_red >= 0 && y_shifted_red < m_height) {
-                            m_data_shifted[0][y*m_width + x] = m_data_original[0][y_shifted_red*m_width + x_shifted_red];
-                        }
+                cv::Mat color_channels_original[3];
+                split(m_data_original, color_channels_original);
 
-                        if (x_shifted_blue >= 0 && x_shifted_blue < m_width && y_shifted_blue >= 0 && y_shifted_blue < m_height) {
-                            m_data_shifted[2][y*m_width + x] = m_data_original[2][y_shifted_blue*m_width + x_shifted_blue];
-                        }
+                cv::Mat color_channels_shifted[3];
+                split(m_data_shifted, color_channels_shifted);
 
-                        m_data_shifted[1][y*m_width + x] = m_data_original[1][y*m_width + x];
-                    }
-                }
-            };
+                cv::warpAffine(color_channels_original[0], color_channels_shifted[0],trans_mat_blue, color_channels_shifted[0].size());
+                cv::warpAffine(color_channels_original[2], color_channels_shifted[2],trans_mat_red, color_channels_shifted[2].size());
 
-            const std::vector<std::vector<PixelType>> &get_original_image() const   {
-                return m_data_original;
-            }
-
-            const std::vector<std::vector<PixelType>> &get_shifted_image() const    {
-                return m_data_shifted;
+                cv::merge(color_channels_shifted, 3, m_data_shifted);
             };
 
             int get_width() const  {
-                return m_width;
+                return m_data_original.cols;
             };
 
             int get_height() const {
-                return m_height;
+                return m_data_original.rows;
             };
 
             void save_shifted_image(const std::string &file_address) const  {
-                create_color_image(m_data_shifted[0].data(), m_data_shifted[1].data(), m_data_shifted[2].data(), m_width, m_height, file_address, CV_16UC3);
+                cv::imwrite(file_address, m_data_shifted);
             };
 
         private:
-            std::vector<std::vector<PixelType>> m_data_original;
-            std::vector<std::vector<PixelType>> m_data_shifted;
-
-            int m_width;
-            int m_height;
+            cv::Mat m_data_original;
+            cv::Mat m_data_shifted;
     };
 }
