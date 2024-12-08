a0f553881f20f0e903dc783fb1f3642228e8b6e7
dubovsky14 <dubovsky14@gmail.com>
2024-12-08 21:43:04 +0100

    Moving getting timestamp from filecreation to a separate function

--- headers/MetadataReader.h
+++ headers/MetadataReader.h
@@ -14,4 +14,6 @@ namespace AstroPhotoStacker {
     Metadata read_metadata(const InputFrame &input_frame);
 
     int get_unix_timestamp(const std::string &time_string);
+
+    Metadata get_file_creation_timestamp(const std::string &file_address, const Metadata &other_metadata = Metadata());
 }
--- src/MetadataReader.cxx
+++ src/MetadataReader.cxx
@@ -43,18 +43,7 @@ Metadata AstroPhotoStacker::read_metadata_video(const std::string &input_file) {
         std::cerr << "Error reading file " << input_file << ": " << e.what() << std::endl;
 
         // get at least the timestamp
-        auto ftime = filesystem::last_write_time(input_file);
-        auto sctp = std::chrono::time_point_cast<std::chrono::system_clock::duration>(
-            ftime - filesystem::file_time_type::clock::now() + std::chrono::system_clock::now()
-        );
-
-        std::time_t cftime = std::chrono::system_clock::to_time_t(sctp);
-
-        // convert to string
-        std::stringstream ss;
-        ss << std::put_time(std::localtime(&cftime), "%Y:%m:%d %H:%M:%S");
-        metadata.date_time = ss.str();
-        metadata.timestamp = get_unix_timestamp(metadata.date_time);
+        metadata = get_file_creation_timestamp(input_file, metadata);
     }
     return metadata;
 }
@@ -134,9 +123,26 @@ Metadata AstroPhotoStacker::read_metadata(const InputFrame &input_frame)    {
     }
 };
 
-
 int AstroPhotoStacker::get_unix_timestamp(const std::string &time_string)  {
     struct tm tm;
     strptime(time_string.c_str(), "%Y:%m:%d %H:%M:%S", &tm);
     return mktime(&tm);
+};
+
+Metadata AstroPhotoStacker::get_file_creation_timestamp(const std::string &file_address, const Metadata &other_metadata)  {
+        Metadata metadata = other_metadata;
+
+        auto ftime = filesystem::last_write_time(file_address);
+        auto sctp = std::chrono::time_point_cast<std::chrono::system_clock::duration>(
+            ftime - filesystem::file_time_type::clock::now() + std::chrono::system_clock::now()
+        );
+        std::time_t cftime = std::chrono::system_clock::to_time_t(sctp);
+
+        // convert to string
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&cftime), "%Y:%m:%d %H:%M:%S");
+        metadata.date_time = ss.str();
+        metadata.timestamp = get_unix_timestamp(metadata.date_time);
+
+        return metadata;
 };
