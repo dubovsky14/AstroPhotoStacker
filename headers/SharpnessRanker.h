0990fb283ee038e8ff4983665b25c4a9641a78d8
dubovsky14 <dubovsky14@gmail.com>
2024-12-13 20:52:39 +0100

    Using RMS for ranking sharpness of the photos

--- headers/SharpnessRanker.h
+++ headers/SharpnessRanker.h
@@ -9,35 +9,26 @@
 namespace AstroPhotoStacker {
     template<class ValueType>
     float get_sharpness_factor(const ValueType *image_data, unsigned int width, unsigned int height, const AlignmentWindow &alignment_window = {-1,-1,-1,-1})    {
-        double sum_weights          = 0;
-        double sum_diff2_weighted   = 0;
-
         const bool use_alignment_window = alignment_window.x_min >= 0 && alignment_window.x_max >= 0 && alignment_window.y_min >= 0 && alignment_window.y_max >= 0;
         const unsigned int x0 = use_alignment_window ? alignment_window.x_min : 0;
         const unsigned int y0 = use_alignment_window ? alignment_window.y_min : 0;
         const unsigned int x1 = use_alignment_window ? alignment_window.x_max : width;
         const unsigned int y1 = use_alignment_window ? alignment_window.y_max : height;
 
+        double mean = 0;
+        double mean2 = 0;
         for (unsigned int y = y0; y < y1; y++)    {
             for (unsigned int x = x0; x < x1; x++)    {
-                // right
-                if (x < width - 1)  {
-                    const double diff = fabs(double(image_data[y * width + x]) - image_data[y * width + x + 1]);
-                    const double weight = std::max(image_data[y * width + x], image_data[y * width + x + 1]);
-                    sum_diff2_weighted += diff * diff * weight;
-                    sum_weights += weight;
-                }
-
-                // down
-                if (y < height - 1)  {
-                    const double diff = fabs(double(image_data[y * width + x]) - image_data[(y + 1) * width + x]);
-                    const double weight = fabs(std::max(image_data[y * width + x], image_data[(y + 1) * width + x]));
-                    sum_diff2_weighted += diff * diff * weight;
-                    sum_weights += weight;
-                }
+                mean += image_data[y * width + x];
+                mean2 += image_data[y * width + x] * image_data[y * width + x];
             }
         }
-        return sum_diff2_weighted / sum_weights;
+        const double n_pixels = (x1 - x0) * (y1 - y0);
+        mean /= n_pixels;
+        mean2 /= n_pixels;
+
+        const double rms = sqrt(mean2 - mean*mean);
+        return rms;
     };
 
 
