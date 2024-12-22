824ba1594b0d768036287968ebd56948251c9efe
dubovsky14 <dubovsky14@gmail.com>
2024-12-22 18:04:32 +0100

    Moving implementation of PostProcessingTool methods to .cxx file

--- GUI/headers/PostProcessingTool.h
+++ GUI/headers/PostProcessingTool.h
@@ -4,72 +4,40 @@
 #include "../../headers/SharpeningFunctions.h"
 
 #include <vector>
-#include <iostream>
 
 class PostProcessingTool    {
     public:
         PostProcessingTool() = default;
 
-        void set_apply_sharpening(bool apply_sharpening) {
-            m_apply_sharpening = apply_sharpening;
-        };
+        void set_apply_sharpening(bool apply_sharpening);
 
-        bool get_apply_sharpening() const {
-            return m_apply_sharpening;
-        };
+        bool get_apply_sharpening() const;
 
-        void set_kernel_size(int kernel_size) {
-            m_kernel_size = kernel_size;
-        };
+        void set_kernel_size(int kernel_size);
 
-        int get_kernel_size() const {
-            return m_kernel_size;
-        };
+        int get_kernel_size() const;
 
-        void set_gauss_width(float gauss_width) {
-            m_gauss_width = gauss_width;
-        };
+        void set_gauss_width(float gauss_width);
 
-        float get_gauss_width() const {
-            return m_gauss_width;
-        };
+        float get_gauss_width() const;
 
-        void set_center_value(float center_value) {
-            m_center_value = center_value;
-        };
+        void set_center_value(float center_value);
 
-        float get_center_value() const {
-            return m_center_value;
-        };
+        float get_center_value() const;
 
-        void set_apply_rgb_alignment(bool apply_rgb_alignment) {
-            m_apply_rgb_alignment = apply_rgb_alignment;
-        };
+        void set_apply_rgb_alignment(bool apply_rgb_alignment);
 
-        bool get_apply_rgb_alignment() const {
-            return m_apply_rgb_alignment;
-        };
+        bool get_apply_rgb_alignment() const;
 
-        void set_rgb_alignment_parameters(const std::pair<int,int> &shift_red, const std::pair<int,int> &shift_blue) {
-            m_shift_red = shift_red;
-            m_shift_blue = shift_blue;
-        };
+        void set_rgb_alignment_parameters(const std::pair<int,int> &shift_red, const std::pair<int,int> &shift_blue);
 
-        void set_shift_red(const std::pair<int,int> &shift_red) {
-            m_shift_red = shift_red;
-        };
+        void set_shift_red(const std::pair<int,int> &shift_red);
 
-        std::pair<int,int> get_shift_red() const {
-            return m_shift_red;
-        };
+        std::pair<int,int> get_shift_red() const;
 
-        void set_shift_blue(const std::pair<int,int> &shift_blue) {
-            m_shift_blue = shift_blue;
-        };
+        void set_shift_blue(const std::pair<int,int> &shift_blue);
 
-        std::pair<int,int> get_shift_blue() const {
-            return m_shift_blue;
-        };
+        std::pair<int,int> get_shift_blue() const;
 
         template<typename PixelType>
         std::vector<std::vector<PixelType>> post_process_image(const std::vector<std::vector<PixelType>> &image, int width, int height) const {
---
+++ GUI/src/PostProcessingTool.cxx
@@ -0,0 +1,66 @@
+#include "../headers/PostProcessingTool.h"
+
+using namespace std;
+using namespace AstroPhotoStacker;
+
+
+void PostProcessingTool::set_apply_sharpening(bool apply_sharpening) {
+    m_apply_sharpening = apply_sharpening;
+};
+
+bool PostProcessingTool::get_apply_sharpening() const {
+    return m_apply_sharpening;
+};
+
+void PostProcessingTool::set_kernel_size(int kernel_size) {
+    m_kernel_size = kernel_size;
+};
+
+int PostProcessingTool::get_kernel_size() const {
+    return m_kernel_size;
+};
+
+void PostProcessingTool::set_gauss_width(float gauss_width) {
+    m_gauss_width = gauss_width;
+};
+
+float PostProcessingTool::get_gauss_width() const {
+    return m_gauss_width;
+};
+
+void PostProcessingTool::set_center_value(float center_value) {
+    m_center_value = center_value;
+};
+
+float PostProcessingTool::get_center_value() const {
+    return m_center_value;
+};
+
+void PostProcessingTool::set_apply_rgb_alignment(bool apply_rgb_alignment) {
+    m_apply_rgb_alignment = apply_rgb_alignment;
+};
+
+bool PostProcessingTool::get_apply_rgb_alignment() const {
+    return m_apply_rgb_alignment;
+};
+
+void PostProcessingTool::set_rgb_alignment_parameters(const std::pair<int,int> &shift_red, const std::pair<int,int> &shift_blue) {
+    m_shift_red = shift_red;
+    m_shift_blue = shift_blue;
+};
+
+void PostProcessingTool::set_shift_red(const std::pair<int,int> &shift_red) {
+    m_shift_red = shift_red;
+};
+
+std::pair<int,int> PostProcessingTool::get_shift_red() const {
+    return m_shift_red;
+};
+
+void PostProcessingTool::set_shift_blue(const std::pair<int,int> &shift_blue) {
+    m_shift_blue = shift_blue;
+};
+
+std::pair<int,int> PostProcessingTool::get_shift_blue() const {
+    return m_shift_blue;
+};
