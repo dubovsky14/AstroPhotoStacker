a460d3ea7a9d2db01c8f33598f9784957b9e0843
dubovsky14 <dubovsky14@gmail.com>
2024-12-24 13:58:31 +0100

    Firecapture to ZWO logs conversion: Adding support for folders

--- python/convert_firecapture_txt_to_zwo_txt.py
+++ python/convert_firecapture_txt_to_zwo_txt.py
@@ -1,4 +1,5 @@
 from sys import argv
+import os
 
 def adjust_values(settings_dict : dict) -> None:
     resolution = settings_dict["ROI"].split("x")
@@ -32,8 +33,29 @@ def convert_firecapture_txt_to_zwo_txt(firecapture_txt_file : str, zwo_txt_file 
         output_file.write(f'White Balance (B) = {settings["WBlue"]}\n')
         output_file.write(f'White Balance (R) = {settings["WRed"]}\n')
 
+def get_list_of_input_files_and_output_files(input_folder : str) -> list[tuple[str, str]]:
+    result = []
+    for file in os.listdir(input_folder):
+        if file.endswith(".txt") and not file.endswith("avi.txt"):
+            input_file = os.path.join(input_folder, file)
+            output_file = input_file[:-4] + ".avi.txt"
+            result.append((input_file, output_file))
+    return result
+
+
 if __name__ == "__main__":
-    if len(argv) < 3:
-        print("Usage: python convert_firecapture_txt_to_zwo_txt.py firecapture.txt zwo.txt")
-        exit(1)
-    convert_firecapture_txt_to_zwo_txt(argv[1], argv[2])
+    if len(argv) == 3:
+        input_file_address = argv[1]
+        output_file_address = argv[2]
+        if not os.path.isfile(input_file_address):
+            files = get_list_of_input_files_and_output_files(input_file_address)
+            print("Usage: python convert_firecapture_txt_to_zwo_txt.py firecapture.txt zwo.txt")
+            exit(1)
+        convert_firecapture_txt_to_zwo_txt(input_file_address, output_file_address)
+    elif len(argv) == 2:
+        if not os.path.isdir(argv[1]):
+            print("Usage: python convert_firecapture_txt_to_zwo_txt.py <directory with firecapture txt files>")
+            exit(1)
+        files = get_list_of_input_files_and_output_files(argv[1])
+        for input_file, output_file in files:
+            convert_firecapture_txt_to_zwo_txt(input_file, output_file)
