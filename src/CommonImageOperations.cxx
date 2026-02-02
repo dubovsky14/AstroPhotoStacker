e3b20dca77d0e46673a54c942d853182cee822cf
dubovsky14 <dubovsky14@gmail.com>
2026-02-02 21:03:54 +0100

    Removing obsolete comments + fixing compiler warning

--- src/CommonImageOperations.cxx
+++ src/CommonImageOperations.cxx
@@ -29,30 +29,27 @@ unsigned short AstroPhotoStacker::get_otsu_threshold(const unsigned short *brigh
 
     double minimal_variation = std::numeric_limits<double>::max();
     unsigned short optimal_threshold = 0;
-    const double total_mean = sum / n_pixels;
 
     for (int threshold = 0; threshold < n_bins; threshold++) {
-        weight_sum_background += histogram[threshold];                  // Weight Background
+        weight_sum_background += histogram[threshold];
         if (weight_sum_background == 0) continue;
 
-        weight_sum_foreground = n_pixels - weight_sum_background;       // Weight Foreground
+        weight_sum_foreground = n_pixels - weight_sum_background;
         if (weight_sum_foreground == 0) break;
 
         sum_background += static_cast<double>(threshold * histogram[threshold]);
         sum2_background += static_cast<double>(threshold * threshold * histogram[threshold]);
 
-        const double mean_background = sum_background / weight_sum_background;        // Mean Background
-        const double mean2_background = sum2_background / weight_sum_background;      // Mean2 Background
+        const double mean_background = sum_background / weight_sum_background;
+        const double mean2_background = sum2_background / weight_sum_background;
 
-        const double mean_foreground = (sum - sum_background) / weight_sum_foreground;        // Mean Foreground
-        const double mean2_foreground = (sum2 - sum2_background) / weight_sum_foreground;      // Mean2 Foreground
+        const double mean_foreground = (sum - sum_background) / weight_sum_foreground;
+        const double mean2_foreground = (sum2 - sum2_background) / weight_sum_foreground;
 
         const double variance_background = mean2_background - mean_background * mean_background;
         const double variance_foreground = mean2_foreground - mean_foreground * mean_foreground;
 
-        // Calculate Between Class Variance
         const double inter_class_variance = (weight_sum_background * variance_background + weight_sum_foreground * variance_foreground);
-        // Check if new maximum found
         if (inter_class_variance < minimal_variation) {
             minimal_variation = inter_class_variance;
             optimal_threshold = threshold;
