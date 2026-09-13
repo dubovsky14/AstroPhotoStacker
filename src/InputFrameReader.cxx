616c3f5f2ec01c79c415b438f401dae5b28bbe0d
dubovsky14 <dubovsky14@gmail.com>
2026-09-13 17:10:28 +0200

    Check if there is an actual valid lens correction for non-debayered raw file, before throwing exception

--- src/InputFrameReader.cxx
+++ src/InputFrameReader.cxx
@@ -180,15 +180,14 @@ void InputFrameReader::read_non_raw() {
 };
 
 void InputFrameReader::apply_lens_correction_if_available()  {
-    if (this->is_raw_file_before_debayering())  {
+    const std::shared_ptr<const LensCorrectionTool> lens_correction_tool = LensCorrectionDictionary::get_instance().get_lens_correction_tool(m_input_frame);
+
+    if (this->is_raw_file_before_debayering() && lens_correction_tool != nullptr)  {
         throw std::runtime_error("Lens correction for raw files before debayering is not supported.");
     }
-    else {
-        const std::shared_ptr<const LensCorrectionTool> lens_correction_tool = LensCorrectionDictionary::get_instance().get_lens_correction_tool(m_input_frame);
-        if (lens_correction_tool != nullptr) {
-            for (auto &channel_data : m_rgb_data) {
-                channel_data = lens_correction_tool->get_undistorted_image(channel_data, m_width, m_height);
-            }
+    else if (lens_correction_tool != nullptr) {
+        for (auto &channel_data : m_rgb_data) {
+            channel_data = lens_correction_tool->get_undistorted_image(channel_data, m_width, m_height);
         }
     }
 };
